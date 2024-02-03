97883f2a302025548c802800c76e4d0a7d13633c
chnmk <a2molchanov@ya.ru>
2024-02-03 13:25:04 +0300

    style: add basic tailwind grid layout

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,33 @@
 <template>
-    <h1 class="my-style shadow-xl"> Hello, Vuejs with Laravel </h1>
+    <div>
+        <div class="fixed inset-x-0 top-0 h-16 bg-indigo-500">
+            <div class="text-center">
+                test
+            </div>
+        </div>
+        <div class="grid grid-cols-3 gap-16 my-32">
+            <div class="bg-emerald-200 hover:bg-emerald-500 col-start-2">
+                <div class="text-xl text-center p-16">
+                    test1
+                </div>
+            </div>
+            <div class="bg-emerald-200 hover:bg-emerald-500 col-start-2">
+                <div class="text-xl text-center p-16">
+                    test2
+                </div>
+            </div>
+            <div class="bg-emerald-200 hover:bg-emerald-500 col-start-2">
+                <div class="text-xl text-center p-16">
+                    test3
+                </div>
+            </div>
+            <div class="bg-emerald-200 hover:bg-emerald-500 col-start-2">
+                <div class="text-xl text-center p-16">
+                    test4
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
 export default {
