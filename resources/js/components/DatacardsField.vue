5bfc6784a9c5bf38e03d654baf8e6b3a6935fbcd
chnmk <a2molchanov@ya.ru>
2024-02-09 23:05:14 +0300

    ref: decompose App.vue template

---
+++ resources/js/components/DatacardsField.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="grid grid-cols-3 gap-16 my-32">
+        <SingleDatacard v-for="card in cards" :key="card.id" :title="card.title" :text="card.text" />
+    </div>
+</template>
+
+<script>
+import SingleDatacard from './SingleDatacard.vue'
+
+export default {
+    data() {
+        return {
+            cards: [
+                { id: 1, title: 'Title 1', text: 'Sample text 1' },
+                { id: 2, title: 'Title 2', text: 'Sample text 2' },
+                { id: 3, title: 'Title 3', text: 'Sample text 3' },
+            ]
+        };
+    },
+    components: {
+        SingleDatacard
+    }
+};
+</script>
