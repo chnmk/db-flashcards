646735393cb92d4c2ee143d334a7d816e75ac1ef
chnmk <a2molchanov@ya.ru>
2024-02-10 13:27:22 +0300

    feat: add basic structure for datacards

--- resources/js/components/DatacardsField.vue
+++ resources/js/components/DatacardsField.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="grid grid-cols-3 gap-16 my-32">
-        <SingleDatacard v-for="card in cards" :key="card.id" :title="card.title" :text="card.text" />
+        <SingleDatacard v-for="card in cards" :key="card.id" :title="card.title" :dl="card.dl" />
     </div>
 </template>
 
@@ -11,9 +11,24 @@ export default {
     data() {
         return {
             cards: [
-                { id: 1, title: 'Title 1', text: 'Sample text 1' },
-                { id: 2, title: 'Title 2', text: 'Sample text 2' },
-                { id: 3, title: 'Title 3', text: 'Sample text 3' },
+                {
+                    id: 1, title: 'Title 1', dl: {
+                        dt: "dt 1",
+                        dd: "dd 1",
+                    }
+                },
+                {
+                    id: 1, title: 'Title 2', dl: {
+                        dt: "dt 2",
+                        dd: "dd 2",
+                    }
+                },
+                {
+                    id: 1, title: 'Title 3', dl: {
+                        dt: "dt 3",
+                        dd: "dd 3",
+                    }
+                },
             ]
         };
     },
--- resources/js/components/SingleDatacard.vue
+++ resources/js/components/SingleDatacard.vue
@@ -1,16 +1,24 @@
 <template>
     <div class="bg-emerald-200 hover:bg-emerald-500 col-start-2 transition duration-300 ease-in-out">
         <div class="text-xl text-center p-16">
-            {{ title }}
-            <br>
-            {{ text }}
+            <h1>
+                {{ title }}
+            </h1>
+            <dl>
+                <dt>
+                    {{ dl.dt }}
+                </dt>
+                <dd>
+                    {{ dl.dd }}
+                </dd>
+            </dl>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['title', 'text']
+    props: ['title', 'dl']
 }
 </script>
 
