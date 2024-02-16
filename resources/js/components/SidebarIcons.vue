c4f71db0e9965113f1ab6812e0c744dfd7434f3e
chnmk <a2molchanov@ya.ru>
2024-02-16 08:25:47 +0300

    style: add navigation template to sidebar

---
+++ resources/js/components/SidebarIcons.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="cursor-pointer hover:bg-yellow-300 transition duration-300 ease-in-out">
+        <svg class="h-16 w-6 mx-6 inline" xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
+            <path
+                d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z" />
+        </svg>
+        <span class="align-middle">
+            Users
+        </span>
+    </div>
+    <div class="cursor-pointer hover:bg-yellow-300 transition duration-300 ease-in-out">
+        <svg class="h-16 w-6 mx-6 inline" xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
+            <path
+                d="M40 48C26.7 48 16 58.7 16 72v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V72c0-13.3-10.7-24-24-24H40zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zM16 232v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V232c0-13.3-10.7-24-24-24H40c-13.3 0-24 10.7-24 24zM40 368c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V392c0-13.3-10.7-24-24-24H40z" />
+        </svg>
+        <span class="align-middle">
+            Collections
+        </span>
+    </div>
+</template>
--- resources/js/components/SidebarPanel.vue
+++ resources/js/components/SidebarPanel.vue
@@ -2,16 +2,23 @@
     <transition enter-from-class="translate-x-[-100%] opacity-0" leave-to-class="translate-x-[-100%] opacity-0"
         enter-active-class="transition duration-500" leave-active-class="transition duration-500">
         <div class="fixed inset-y-0 top-16 w-64 h-full bg-yellow-200" v-if="sidebarOpen">
+            <SidebarIcons />
+            <hr class="mx-6 border-yellow-500" />
             <div class="text-center">
-                <div class="my-4" v-if="sidebarOpen">sample sidebar</div>
-                <div class="my-4" v-if="sidebarOpen">sample sidebar</div>
+                <div class="my-4">sample sidebar</div>
+                <div class="my-4">sample sidebar</div>
             </div>
         </div>
     </transition>
 </template>
 
 <script>
+import SidebarIcons from './SidebarIcons.vue'
+
 export default {
-    props: ['sidebarOpen']
+    props: ['sidebarOpen'],
+    components: {
+        SidebarIcons
+    }
 }
 </script>
