a9dd9092a887c025b1f88a5910c7694b3cf8588f
chnmk <a2molchanov@ya.ru>
2024-02-17 12:08:52 +0300

    style: add basic sidebar item style

---
+++ resources/js/components/SidebarItem.vue
@@ -0,0 +1,12 @@
+<template>
+    <div
+        class="my-4 mx-10 py-2 border-b-2 border-yellow-300 text-center cursor-pointer hover:bg-yellow-300 transition duration-300 ease-in-out">
+        {{ name }}
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['name']
+}
+</script>
--- resources/js/components/SidebarPanel.vue
+++ resources/js/components/SidebarPanel.vue
@@ -2,23 +2,46 @@
     <transition enter-from-class="translate-x-[-100%] opacity-0" leave-to-class="translate-x-[-100%] opacity-0"
         enter-active-class="transition duration-500" leave-active-class="transition duration-500">
         <div class="fixed inset-y-0 top-16 w-64 h-full bg-yellow-200" v-if="sidebarOpen">
-            <SidebarIcons />
+            <SidebarNav />
             <hr class="mx-6 border-yellow-500" />
-            <div class="text-center">
-                <div class="my-4">sample sidebar</div>
-                <div class="my-4">sample sidebar</div>
-            </div>
+            <SidebarItem v-for="c in collections" :key="c.id" :name="c.name" />
         </div>
     </transition>
 </template>
 
 <script>
-import SidebarIcons from './SidebarIcons.vue'
+import SidebarNav from './SidebarNav.vue'
+import SidebarItem from './SidebarItem.vue'
 
 export default {
     props: ['sidebarOpen'],
     components: {
-        SidebarIcons
+        SidebarNav,
+        SidebarItem
+    },
+    data() {
+        return {
+            collections: [
+                {
+                    id: 1,
+                    name: "Default"
+                },
+                {
+                    id: 2,
+                    name: "My Collection"
+                },
+            ],
+            users: [
+                {
+                    id: 1,
+                    name: "Edward"
+                },
+                {
+                    id: 2,
+                    name: "Serj"
+                },
+            ]
+        }
     }
 }
 </script>
