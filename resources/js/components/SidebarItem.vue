ec87d7683cb04258bf1cb349ee5a9b55ff8e3a52
chnmk <a2molchanov@ya.ru>
2024-02-23 15:23:44 +0300

    feat: add router link template to sidebar item

--- resources/js/components/SidebarItem.vue
+++ resources/js/components/SidebarItem.vue
@@ -1,8 +1,13 @@
 <template>
-    <div
-        class="my-4 mx-10 py-2 border-b-2 border-yellow-300 text-center cursor-pointer hover:bg-yellow-300 transition duration-300 ease-in-out">
-        {{ name }}
-    </div>
+    <router-link :to="{
+        name: 'home', params: { user: 1, id: 1 }
+    }">
+        <div
+            class="my-4 mx-10 py-2 border-b-2 border-yellow-300 text-center cursor-pointer hover:bg-yellow-300 transition duration-300 ease-in-out">
+
+            {{ name }}
+        </div>
+    </router-link>
 </template>
 
 <script>
